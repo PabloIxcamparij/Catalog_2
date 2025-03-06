6964dd67b1f84c729788552753c417efec76724e
PabloIxcamparij <j.pablo.sorto@gmail.com>
2025-03-05 18:40:42 -0600

    feat: TailwindCSS
    - Has been integrated into the project tailwindcss

--- src/layout/LayoutMain.tsx
+++ src/layout/LayoutMain.tsx
@@ -2,7 +2,7 @@ import { NavLink, Outlet } from "react-router-dom";
 
 export default function LayoutMain() {
   return (
-    <div>
+    <div className="flex flex-col">
       LayoutMain
       <NavLink to={"/"}>Home</NavLink>
       <NavLink to={"/about"}> About Product</NavLink>
